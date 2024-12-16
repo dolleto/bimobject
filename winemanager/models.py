e55f69b1ce420bd4014b8757d0add11307fa7d20
dolleto <alexandre.dolle@klarna.com>
2024-12-16 08:56:35 +0100

    feat: lift requirements on some model fields

--- winemanager/models.py
+++ winemanager/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Winemaker(models.Model):
+    """
+    Stores a single wine maker entry.
+    A wine producer does not need to have a relation with a wine bottle.
+    """
+
     name = models.CharField(max_length=255)
     address = models.TextField()
 
@@ -10,7 +15,14 @@ class Winemaker(models.Model):
 
 
 class WineBottle(models.Model):
-    STYLE_CHOICES = [
+    """
+    Stores a single wine bottle entry, related to :model: winemaker.Winemaker.
+    Notes:
+        - A wine bottle can only belong to one wine maker.
+        - Only some properties set to be required: name, year, size, count_in_winecellar, style.
+    """
+
+    STYLE_CHOICES = [  # Basic choices for wine style
         ("dry", "Dry"),
         ("sweet", "Sweet"),
     ]
@@ -21,10 +33,13 @@ class WineBottle(models.Model):
     size = models.CharField(max_length=50)
     count_in_winecellar = models.PositiveIntegerField()
     style = models.CharField(max_length=50, choices=STYLE_CHOICES)
-    taste = models.TextField(help_text="Comma separated, e.g., plum, tobacco")
-    description = models.TextField()
-    food_pairing = models.TextField()
-    link = models.URLField(blank=True, null=True)
+    taste = models.TextField(help_text="Comma separated, e.g., plum, tobacco", blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    food_pairing = models.TextField(help_text="Comma separated, e.g. red meat, cheese", blank=True, null=True)
+    link = models.URLField(
+        help_text="Unique URL for each wine bottle (could be a link to the wine maker's website)", blank=True, null=True
+    )
+
     # TODO: Support image field
     # image = models.ImageField(upload_to="images/", blank=True, null=True)
 
